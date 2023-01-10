623f74da4ac3288aa990be72eee91549f3534c6d
pcarette <pierre.carette78@gmail.com>
2023-01-10 21:57:54 +0100

    FIX: signOut function implemented in Redux Saga

--- src/store/user/user.action.js
+++ src/store/user/user.action.js
@@ -32,11 +32,12 @@ export const signUpSucces = (user, additionalDetails) =>
 
 export const signUpFailed = (error) =>
   createAction(USER_ACTIONS_TYPES.SIGN_UP_FAILED, error);
-// export const USER_ACTIONS_TYPES = {
-//     SET_CURRENT_USER: "user/SET_CURRENT_USER",
-//     CHECK_USER_SESSION: "user/CHECK_USER_SESSION",
-//     GOOGLE_SIGN_IN_START: "user/GOOGLE_SIGN_IN_START",
-//     EMAIL_SIGN_IN_START: "user/EMAIL_SIGN_IN_START",
-//     SIGN_IN_SUCCESS : 'user/SIGN_IN_SUCCESS',
-//     SIGN_IN_FAILURE : 'user/SIGN_IN_FAILURE',
-//   };
+
+export const signOutStart = () =>
+  createAction(USER_ACTIONS_TYPES.SIGN_OUT_START);
+
+export const signOutSuccess = () =>
+  createAction(USER_ACTIONS_TYPES.SIGN_OUT_SUCCESS);
+
+export const signOutFailed = (error) =>
+  createAction(USER_ACTIONS_TYPES.SIGN_OUT_FAILED, error);
--- src/store/user/user.reducer.js
+++ src/store/user/user.reducer.js
@@ -3,19 +3,21 @@ import { USER_ACTIONS_TYPES } from "./user.types";
 const USER_INITIAL_STATE = {
   currentUser: null,
   isLoading: false,
-  error : null,
+  error: null,
 };
 
 export const userReducer = (state = USER_INITIAL_STATE, action = {}) => {
   const { type, payload } = action;
 
   switch (type) {
-    case USER_ACTIONS_TYPES.SET_CURRENT_USER:
-      return { ...state, currentUser : payload }
     case USER_ACTIONS_TYPES.SIGN_IN_SUCCESS:
       return { ...state, currentUser: payload };
-    case USER_ACTIONS_TYPES.SIGN_IN_FAILED:
-      return { ...state, error : payload }
+    case USER_ACTIONS_TYPES.SIGN_OUT_SUCCESS:
+      return { ...state, currentUser: null };
+    case USER_ACTIONS_TYPES.SIGN_OUT_FAILED:
+    case USER_ACTIONS_TYPES.SIGN_IN_FAILURE:
+    case USER_ACTIONS_TYPES.SIGN_UP_FAILED:
+      return { ...state, error: payload };
     default:
       return state;
   }
